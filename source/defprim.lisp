25f62e9dc083c1ac5ab24718f092e78f421eff16
Thomas Bartscher <none@none>
2013-05-17 18:36:59 +0200

    DEFPRIM added.
    --HG--
    branch : refactor

--- source/defprim.lisp
+++ source/defprim.lisp
@@ -6,3 +6,8 @@
 	       (remove ',name *prims*
 		       :test #'eq
 		       :key #'car))))
+
+(defmacro defprim (name immediate? docstring &body body)
+  `(defnprim ,name ,immediate? ,docstring
+     (advance pc)
+     ,@body))
