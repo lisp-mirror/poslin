487393373ed66f20a3c6147285fea7870755aa8e
Thomas Bartscher <thomas.bartscher@weltraumschlangen.de>
2014-08-23 12:37:15 +0200

    Changed FLATTEN to be compatible with SBCL's new way of handling commata.

--- utility/flatten.lisp
+++ utility/flatten.lisp
@@ -1,8 +1,20 @@
 (in-package #:poslin)
 
 (defun flatten (structure)
+  #+sbcl
+  (typecase structure
+    (cons
+     (apply #'append
+	    (mapcar #'flatten
+		    structure)))
+    (sb-impl::comma
+     (flatten (sb-impl::comma-expr structure)))
+    (t
+     (list structure)))
+  #-sbcl
   (if (consp structure)
       (apply #'append
 	     (mapcar #'flatten
 		     structure))
-      (list structure)))
+      (list structure))
+  )
